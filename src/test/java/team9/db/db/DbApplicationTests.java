fbedd2db1a57985b73ab2142bea84b286f4b7d7b
e1b20054 <e1b20054@oit.ac.jp>
2022-10-18 14:07:22 +0900

    リポジトリの初期化

---
+++ src/test/java/team9/db/db/DbApplicationTests.java
@@ -0,0 +1,13 @@
+package team9.db.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
